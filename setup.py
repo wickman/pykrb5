80316eb57520cf40ac6f4215f81989cb4799c63a
Brian Wickman <wickman@twitter.com>
2015-01-08 12:22:48 -0800

    Make it work.

--- setup.py
+++ setup.py
@@ -10,18 +10,21 @@ setup(
   description='pure python kerberos 5 implementation',
   url='http://github.com/mhorowitz/pykrb5',
   author='Marc Horowitz',
-  packages=['krb5'],
+  packages=[
+      'krb5',
+      'krb5.bin',
+  ],
   install_requires=[
-    'pyDes==0.2.1',
-    'pyasn1==0.0.12a',
-    'flufl.enum==3.1',
+      'pyDes==2.0.1',
+      'pyasn1==0.0.12a',
+      'flufl.enum==3.1',
   ],
   entry_points = {
-    'console_scripts': [
-      'kdestroy=krb5.bin.kdestroy:main',
-      'kinit=krb5.bin.kinit:main',
-      'kvno=krb5.bin.kvno:main',
-      'klist=krb5.bin.klist:main',
-    ],
+      'console_scripts': [
+          'kdestroy=krb5.bin.kdestroy:main',
+          'kinit=krb5.bin.kinit:main',
+          'kvno=krb5.bin.kvno:main',
+          'klist=krb5.bin.klist:main',
+      ],
   }
 )
